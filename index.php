7d7b4a1daae3195a779103ef552366f4706d12bc
Daniel Ryan <daniel.ryan@students.itb.ac.id>
2018-10-14 22:15:53 +0700

    Change url routing implementation in index.php

--- index.php
+++ index.php
@@ -1,18 +1,23 @@
 <?php
-	$url = explode("/", $_SERVER['REQUEST_URI']);
-	$controller = $url[2];
-	$_SERVER['REQUEST_URI'] = "/".implode("/", array_slice($url, 3));
+	$request_url = explode("/", $_SERVER['REQUEST_URI']);
+	$controller = $request_url[2];
+	$_SERVER['REQUEST_URI'] = "/".implode("/", array_slice($request_url, 3));
 	set_include_path($_SERVER["DOCUMENT_ROOT"]."/tugasbesar1_2018/");
 
-	switch ($controller) {
-		case 'login':
-			require "controllers/login.php";
+	$available_urls = array(
+		"login",
+		"register"
+	);
+	
+	$url_matched = false;
+	foreach ($available_urls as $url) {
+		if ($url == $controller) {
+			require "controllers/".$url.".php";
+			$url_matched = true;
 			break;
-		case 'register':
-			require "controllers/register.php";
-			break;
-		default:
-			echo "Nothing to see.";
-			break;
+		}
+	}
+	if (!$url_matched) {
+		echo "Nothing to see.";
 	}
 ?>
