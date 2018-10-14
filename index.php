07cbebb4f62abced3b0e73a905447d89b11b5f16
Daniel Ryan <daniel.ryan@students.itb.ac.id>
2018-10-14 22:02:45 +0700

    Add simple login page (not including logic)

--- index.php
+++ index.php
@@ -2,8 +2,12 @@
 	$url = explode("/", $_SERVER['REQUEST_URI']);
 	$controller = $url[2];
 	$_SERVER['REQUEST_URI'] = "/".implode("/", array_slice($url, 3));
+	set_include_path($_SERVER["DOCUMENT_ROOT"]."/tugasbesar1_2018/");
 
 	switch ($controller) {
+		case 'login':
+			require "controllers/login.php";
+			break;
 		default:
 			echo "Nothing to see.";
 			break;
