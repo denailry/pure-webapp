a8ffd29a5a69fa9b6b7b9ef4fd5c3dbe0b82831c
Daniel Ryan <daniel.ryan@students.itb.ac.id>
2018-10-15 19:55:24 +0700

    create layout of views/login.php

--- views/login.php
+++ views/login.php
@@ -2,21 +2,42 @@
 <html>
     <head>
         <title>Login</title>
+        <link rel="stylesheet" type="text/css" href="statics/css/login.css">
     </head>
     <body>
-        <form method="POST">
-            <div>
-                <?php getvar("failure"); ?>
+        <div id="box-wrapper">
+            <div id="box-container">
+                <div style="text-align: center;">
+                    <h1>Login</h1>
+                </div>
+                <form method="POST">
+                    <div class="input-group">
+                        <div class="input-field">
+                            <label for="username">Username</label>    
+                        </div>
+                        <div class="input-value">
+                            <input id="username" type="text" name="username">
+                        </div>
+                    </div>
+                    <div class="input-group">
+                        <div class="input-field">
+                            <label for="password">Password</label>
+                        </div>
+                        <div class="input-value">
+                            <input id="password" type="password" name="password">
+                        </div>
+                    </div>
+                    <div id="failure-notif" class="box-center">
+                        <?php getvar("failure"); ?>
+                    </div>
+                    <div style="margin-top: 5px; margin-bottom: 10px;">
+                        <a href="register.php">Dont't have an account?</a>
+                    </div>
+                    <div class="box-center">
+                        <button name="submit">Login</button>
+                    </div>
+                </form>
             </div>
-            <div>
-                <label for="username">Username:</label>
-                <input id="username" type="text" name="username">
-            </div>
-            <div>
-                <label for="password">Password:</label>
-                <input id="password" type="password" name="password">
-            </div>
-            <button name="submit">Submit</button>
-        </form>
+        </div>
     </body>
 </html>
