a70ce16ccc631bc52c5afb489025e10999d8212b
prisilamichelle <prisilamichelle@yahoo.com>
2018-10-20 00:59:28 +0700

    add session and cookie

--- profile.php
+++ profile.php
@@ -3,15 +3,25 @@
     require_once "models/user.php";
     require_once "utils/common.php";
     require_once "utils/page_var.php";
+    require_once "utils/validation.php";
 
     embed("main-bar");
 
-    /*$user = $SESSION->get_user();*/
-    $user = new User(1);
-    setvar('username',$user->username);
-    setvar('email',$user->email);
-    setvar('address',$user->address);
-    setvar('phone',$user->phone);
+    $SESSION = null;
+
+    if (isset($_COOKIE['access_token'])) {
+        $SESSION = Session::verify($_COOKIE['access_token']);
+    }
+
+    if ($SESSION == null) {
+        force_login();
+    } else {
+        $user = $SESSION->get_user();
+        setvar('username',$user->username);
+        setvar('email',$user->email);
+        setvar('address',$user->address);
+        setvar('phone',$user->phone);
+    }
     
     include "views/profile.php";
 ?>
