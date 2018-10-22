3b36c828d9c91650676c90cef199fb62f138c50a
prisilamichelle <prisilamichelle@yahoo.com>
2018-10-22 23:06:17 +0700

    add book detail view page, style profile page

---
+++ views/book_detail.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Book Detail</title>
+        <link rel="stylesheet" type="text/css" href="statics/css/home.css">
+    </head>
+    <body>
+        <div id="main">
+            <figure>
+                <img id="cover" src=<?php getvar('cover'); ?>>
+                <figcaption class="caption"><?php getvar('rating'); ?>/5.0</figcaption>
+            </figure>
+            <div>
+                <h3><?php getvar('title'); ?></h3>
+                <span class="author"><?php getvar('author'); ?></span>
+                <span class="detail"><?php getvar('detail'); ?></span>
+            </div>
+            <div class="order">
+                <span class="subheading">Order</span>
+                <button id="btn-order" class="btn-primary">Order</button>
+                <div>
+                    <span class="jumlah">Jumlah :</span>
+                    <select>
+                        <?php
+                            for ($i=1;$i<=100;$i++) {
+                                ?>
+                                    <option value="<?php echo $i;?>"><?php echo $i;?></option>
+                                <?php
+                            }
+                        ?>
+                    </select>
+                </div>
+            </div>
+            <div class="review">
+                <span class="subheading">Reviews</span>
+                <?php 
+                    foreach ($reviews as $item) {
+                        ?>
+                        <table class="reviews">
+                            <td class="profile_picture"><img src=<?php echo $item['profile_picture']; ?>></td>
+                            <td class="comments">
+                                <span class="username"><?php echo "@".$item['username']; ?></span>
+                                <span class="comment"><?php echo $item['comment']; ?></span>
+                            </td>
+                            <td class="ratings"><p><?php echo $item['rating']; ?>/5.0</p></td>
+                        </table>
+                        <?php
+                    }
+                ?>                
+            </div>
+        </div>
+    </body>
+</html>
--- views/profile.php
+++ views/profile.php
@@ -7,37 +7,35 @@
     <body>
         <div id="main">
             <div id="profile">
-                <img id="edit-profile" style="height: 100%; cursor: pointer;" src="statics/img/edit-profile.png"
-                    onclick="window.location.href='edit_profile.php';">
+                <img id="edit-profile" src="statics/img/edit-profile.png"
+                    onclick="window.location.href='editprofile.php';">
             </div>
-            <div style="background-color: #00374c">
-                <img id="profile-picture" src="statics/img/default-profile-picture.png">
+            <div id="profile-picture">
+                <img src="statics/img/default-profile-picture.png">
             </div>
             <div id="profile-name">
                 <span class="name"><?php getvar('name'); ?></span>
             </div>
             <h2>My Profile</h2>
-            <table style="table-layout:fixed; width: 70%; margin-left: 50px">
+            <table class="profile">
                 <tr>
-                    <td style="width: 5px">
-                        <img id="username" style="height: 100%" src="statics/img/username.png">
-                    </td>
-                    <td style="width: 40px">Username</td>
+                    <td class="icon"><img id="username" src="statics/img/username.png"></td>
+                    <td class="title">Username</td>
                     <td>@<?php getvar('username'); ?></td>
                 </tr>
                 <tr>
-                    <td><img id="email" style="height: 100%" src="statics/img/email.png"></td>
-                    <td>Email</td>
+                    <td class="icon"><img id="email" src="statics/img/email.png"></td>
+                    <td class="title">Email</td>
                     <td><?php getvar('email'); ?></td>
                 </tr>
                 <tr>
-                    <td><img id="address" style="height: 100%" src="statics/img/address.png"></td>
-                    <td>Address</td>
+                    <td class="icon"><img id="address" src="statics/img/address.png"></td>
+                    <td class="title">Address</td>
                     <td><?php getvar('address'); ?></td>
                 </tr>
                 <tr>
-                    <td><img id="phone" style="height: 100%" src="statics/img/phone.png"></td>
-                    <td>Phone Number</td>
+                    <td class="icon"><img id="phone" src="statics/img/phone.png"></td>
+                    <td class="title">Phone Number</td>
                     <td><?php getvar('phone'); ?></td>
                 </tr>
             </table>
