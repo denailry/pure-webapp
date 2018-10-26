442ca64daa39769652f77b682f4f579e9b7ef9ec
Mathias Novianto <mathiasnn98@gmail.com>
2018-10-26 10:08:08 +0700

    added font

--- views/search.php
+++ views/search.php
@@ -10,7 +10,9 @@
             <div id="box-search">
                 <h1>Search Book</h1>
                 <form action="/tugasbesar1_2018/search_result.php" method="GET" autocomplete="off">
-                    <input id="input-search" type="text" name="input-search" placeholder="Input search terms...">
+                    <input id="input-search" type="text" name="input-search" placeholder="Input search terms..." required 
+                        oninvalid="this.setCustomValidity('Please input search terms first.')"
+                        oninput="this.setCustomValidity('')">
                     <button id="btn-search" class="btn-primary" type="submit">Search</button>
                 </form>
             </div>
